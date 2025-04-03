a000bd71b6a692d55e4dca1b5f5d27265e7acef1
Alexander <spaco7944@gmail.com>
2025-04-03 12:13:01 +0100

    Subida Ejercicios 2

--- script.js
+++ script.js
@@ -1,47 +1,61 @@
-// Apartado1
-function esVocal(letra) {
+// // Apartado1
+// function esVocal(letra) {
     
-    const letraMinuscula = letra.toLowerCase();
-    return vocales.includes(letraMinuscula);
-  }
+//     const letraMinuscula = letra.toLowerCase();
+//     return vocales.includes(letraMinuscula);
+//   }
   
-  // Ejemplos de uso:
-  vocales = ['a', 'e', 'i', 'o', 'u'];
-  console.log(esVocal('A', 'e'));
+//   // Ejemplos de uso:
+//   vocales = ['a', 'e', 'i', 'o', 'u'];
+//   console.log(esVocal('A', 'e'));
   
-// Apartado2
+// // Apartado2
 
-function procesarArray() {
-    numeros = ['1', '2', '-3', '-1', '2', '4', '4', '1', '45', '-1']
+// function procesarArray() {
+//     numeros = ['1', '2', '-3', '-1', '2', '4', '4', '1', '45', '-1']
     
-    let unicos = [];
-    numeros.forEach(num => {
-        if (!unicos.some(el => el === num)) {
-            unicos.push(num);
-        }
-        return unicos;
-    });
-    unicos.sort((a, b) => a - b);
-    console.log(unicos.toString());
-}
+//     let unicos = [];
+//     numeros.forEach(num => {
+//         if (!unicos.some(el => el === num)) {
+//             unicos.push(num);
+//         }
+//         return unicos;
+//     });
+//     unicos.sort((a, b) => a - b);
+//     console.log(unicos.toString());
+// }
 
-console.log(procesarArray());
+// console.log(procesarArray());
 
 // Apartado3
 
 let clasificaciones = ["Ana", "Oswaldo", "Raúl", "Celia", "María", "Antonio"];
-console.log("Clasificaciones:", clasificaciones);
-let Cindex = clasificaciones.indexOf("Celia");
-let Rindex = clasificaciones.indexOf("Raúl");
-if (Rindex > -1 && Cindex > -1) {
+console.log("Clasificación inicial: " + clasificaciones);
+let antonio = clasificaciones.pop();
+let maria = clasificaciones.pop();
+let celia = clasificaciones.pop();
+let raul = clasificaciones.pop();
 
-    [clasificaciones[Cindex], 
-    clasificaciones[Rindex]] = [clasificaciones[Rindex], 
-    clasificaciones[Cindex]];
-    
-}
+clasificaciones.push(celia);
+clasificaciones.push(raul);
+clasificaciones.push(maria);
+clasificaciones.push(antonio);
 
-let Aindex = clasificaciones.indexOf("Ana");
-clasificaciones.splice(Aindex + 1, 0, "Roberto", "Ana");
+clasificaciones.pop();
+
+let maria2 = clasificaciones.pop();
+let raul2 = clasificaciones.pop();
+let celia2 = clasificaciones.pop();
+let oswaldo = clasificaciones.pop();
+
+clasificaciones.push("Roberto");
+clasificaciones.push("Amaya");
+
+clasificaciones.push(oswaldo);
+clasificaciones.push(celia2);
+clasificaciones.push(raul2);
+clasificaciones.push(maria2);
+
 clasificaciones.unshift("Marta");
-console.log("Ultima clasificacion:", clasificaciones);
+
+console.log("\nClasificación actualizada: " + clasificaciones);
